ff3315bf9c46ed48776fe0ae7395063cb4892995
Aleksei Gazov <lekha.gazov@yandex.ru>
2022-12-11 20:12:48 +0300

    create widthRouter and css mobule component dialogs

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,27 @@ import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
 import Dialogs from "./components/Dialogs/Dialogs";
+import {BrowserRouter, Route} from "react-router-dom";
 
 function App() {
-  return (
-    <div className="app-wrapper">
-        <Header />
-        <Navbar />
-        <Profile />
-        {/*<div className="content">*/}
-        {/*    <Dialogs />*/}
-        {/*</div>*/}
+    return (
+        <BrowserRouter>
+            <div className="app-wrapper">
+                <Header/>
+                <Navbar/>
+                <div className="content">
+                    <Route path="/profile" component={Profile}/>
+                    <Route path='/dialogs' component={Dialogs}/>
+                    {/*<Route path=# component={News}/>*/}
+                    {/*<Route path='/dialogs' component={Music}/>*/}
 
-    </div>
-  );
+
+                </div>
+
+            </div>
+        </BrowserRouter>
+
+    );
 }
+
 export default App;
--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -4,8 +4,31 @@ import s from "./Dialogs.module.css"
 type DialogsType = any
 const Dialogs = (props: DialogsType) => {
     return (
-       <div >
-           messages
+       <div className={s.dialogs}>
+           <div className={s.dialogsItem}>
+              <div className={s.dialog}>
+                  Aleks
+              </div>
+               <div className={s.dialog + ' ' + s.active}>
+                  Andrey
+              </div>
+               <div className={s.dialog}>
+                  Vasia
+              </div>
+               <div className={s.dialog}>
+                  Lili
+              </div>
+                <div className={s.dialog}>
+                  Sisi
+              </div>
+
+           </div>
+           <div className={s.messages}>
+               <div className={s.message}>Hi</div>
+               <div className={s.message}>Hou are yuo?</div>
+               <div className={s.message}>Im faindf</div>
+
+           </div>
        </div>
     )
 }
